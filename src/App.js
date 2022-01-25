faa4ee37415bd1e71a3fe1230313cdfc0b7adfa0
Eugene Sukhovarov <mcboodvk@gmail.com>
2022-01-26 00:14:26 +0300

    Sidebar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import React from "react"
+import SortButton from './Components/SortButtons/sortButton';
 import './App.css';
+
+import DB from "./assets/DB.json"
+import UserBlock from "./Components/UserBlock/UserBlock";
+let obj = JSON.parse(DB)
 
 function App() {
   return (
-    <div>1</div>
+    <div className='usersList'>
+      <div className='usersList__sidebar'>
+      <span>Сортировка</span>
+      <SortButton text="по городу" />
+      <SortButton text="по компании"/>
+      </div>
+      <div className="usersList__lists">
+        <h3>Список пользователей</h3>
+        <div className="usersList__items">
+          <UserBlock props={obj}/>
+        </div>
+      </div>
+    </div>
   );
 }
 
